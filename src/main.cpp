02301254955ab84980c9b76e204e043d50e584d2
danillitucinka-stack <danillitucinka@gmail.com>
2026-09-23 19:04:37 +0300

    Fix Windows build: include algorithm and qualify std::max

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <wrl.h>
 #include <WebView2.h>
 
+#include <algorithm>
 #include <cstddef>
 #include <cwctype>
 #include <string>
@@ -83,7 +84,7 @@ void LayoutChrome() {
     HWND newTab = GetDlgItem(g_window, kNewTabId);
     if (newTab) SetWindowPos(newTab, nullptr, 12 + static_cast<int>(g_tabs.size()) * 170, 5, 28, 25, SWP_NOZORDER);
     if (g_address) SetWindowPos(g_address, nullptr, 172, kTabBarHeight + 9,
-        max(220, bounds.right - 300), 28, SWP_NOZORDER);
+        std::max(220, bounds.right - 300), 28, SWP_NOZORDER);
     if (g_status) SetWindowPos(g_status, nullptr, 8, kToolbarHeight - 20, 160, 18, SWP_NOZORDER);
     ResizeBrowser();
 }
